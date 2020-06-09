190c6302d09ff42b5d94c62f444f526e1c3780dc
johnny <huynhtuandat.ueh@gmail.com>
2020-06-10 03:41:36 +1000

    menu overlay for desktop

--- responsive-portfolio-SASS/dist/js/main.js
+++ responsive-portfolio-SASS/dist/js/main.js
@@ -4,30 +4,11 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-// set initial state of menu
-let showMenu = false;
-
 menuBtn.addEventListener('click', toggleMenu);
-
-
 function toggleMenu() {
-    if(!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
-        //reset the menu state
-        showMenu = true;
-    } else {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
-        //reset the menu state
-        showMenu = false;
-        
-        
-    }
+        menuBtn.classList.toggle('close');
+        menu.classList.toggle('show');
+        menuNav.classList.toggle('show');
+        menuBranding.classList.toggle('show');
+        navItems.forEach(item => item.classList.toggle('show'));
 }
